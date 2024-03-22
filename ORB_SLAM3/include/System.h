e4e8a96bf5fa66bd923483f8c449de1529096523
d4n13lx13 <danxie2001@gmail.com>
2024-03-22 08:26:31 -0400

    working map_node, rviz config, etc.

--- ORB_SLAM3/include/System.h
+++ ORB_SLAM3/include/System.h
@@ -186,6 +186,10 @@ public:
 
     float GetImageScale();
 
+    Atlas* GetAtlas();
+
+    Tracking* GetTracker();
+
 #ifdef REGISTER_TIMES
     void InsertRectTime(double& time);
     void InsertResizeTime(double& time);
