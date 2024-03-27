55daff717461825ffc917ba93d9bde94ef22276b
d4n13lx13 <danxie2001@gmail.com>
2024-03-26 22:08:21 -0400

    updated description

--- map_processor/src/process_map.py
+++ map_processor/src/process_map.py
@@ -1,3 +1,19 @@
 import rosbag2_py
 import sys
+import os
+import pathlib as Path
+import sklearn.cluster._dbscan
 
+'''
+Purpose of this script is to process the map data from an arbitrary ros2 bag and output a cleaned map and provide the cleaned map to pure pursuit node
+
+Input:
+    /map_refined_points
+Ouput:
+    /map_cleaned
+    or 
+    image of the map
+
+Reading the bag file and extracting the map data
+
+'''
