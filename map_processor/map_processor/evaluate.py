6e4ad49c1f6fcb4a4eaba3e628bb429e833f3774
d4n13lx13 <danxie2001@gmail.com>
2024-03-29 01:28:34 -0400

    tuning params

--- map_processor/map_processor/evaluate.py
+++ map_processor/map_processor/evaluate.py
@@ -1,16 +1,13 @@
 import hdbscan
 from sklearn.datasets import make_blobs
+import matplotlib.pyplot as plt
 
 data, _ = make_blobs(1000,3)
 
 clusterer = hdbscan.HDBSCAN(min_cluster_size=10)
 cluster_labels = clusterer.fit_predict(data)
 
-
-import matplotlib.pyplot as plt
-
-print(len(cluster_labels))
-
+print(cluster_labels)
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
 ax.scatter(data[:, 0], data[:, 1], data[:, 2], c=cluster_labels)
